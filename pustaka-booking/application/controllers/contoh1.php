6af30aec63fa0b5945e62f87d2a19d73c8698715
Neezuumii <anandaihlasulamal@gmail.com>
2023-09-25 15:06:39 +0700

    menambahkan folder pustaka-booking

---
+++ pustaka-booking/application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Ananda Ihlasul Amal
+              Saya tinggal di Daerah Tomang
+              Olahraga yang saya sukai adalah
+              BasketBall";
+    }
+}
