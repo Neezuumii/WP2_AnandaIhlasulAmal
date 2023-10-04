f44afd293ace9cc9359631257a0c7ea3fa0b0f73
Neezuumii <anandaihlasulamal@gmail.com>
2023-10-04 14:35:28 +0700

    menambhakan file matakuliah dari view model dan controller

---
+++ pustaka-booking/application/views/view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Latihan 1</title>
+    </head>
+    <body>
+        Halo Kawan.. Yuk Kita Belajar Web Programming..!!!<br>
+        Nilai 1  = <?= $nilai1; ?> <br>
+        Nilai 2  = <?= $nilai2; ?> <br>
+        Ini Hasil dari Pemodelan dengan Methode Penjumlahan yaitu
+        <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    </body>
+</html>
